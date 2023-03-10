c1b74a17d668f1c5cc60f2b3e8480b07ba0978a2
yiwwhl <yiwwhl@qq.com>
2023-03-10 23:06:28 +0800

    chore: arcodesign auto import style

--- packages/react-arco-playground/src/App.tsx
+++ packages/react-arco-playground/src/App.tsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
-import '@arco-design/web-react/dist/css/arco.css'
 import { Button } from '@arco-design/web-react'
 
 function App() {
